25420eecb6eef9c4cba10aab6a2e9dc64d87a10f
Mr Rahul <ashish227087@gmail.com>
2025-04-02 14:00:38 +0530

    JavaScript Data type

---
+++ project1/script.js
@@ -0,0 +1,74 @@
+
+let length = 16;
+let weight = 7.5;
+
+document.getElementById('demo1').innerHTML= `
+// Number// <br>
+let length = 16;<br>
+let weight = 7.5;
+
+`
+let color ="red";
+let lastName ="Verma";
+document.getElementById('demo2').innerHTML=`
+
+// String // <br>
+let color ="red";<br>
+let lastName ="Verma";
+
+`
+
+
+var  x = true;
+ var  y = false;
+document.getElementById('demo3').innerHTML=`
+// Booleans <br>
+var  x = true; <br>
+var  y = false;
+
+`
+
+const person = {
+
+     FirstName:"ASHISH",
+     LastName :"Verma"
+};
+ document.getElementById('demo4').innerHTML=`
+
+ // Obbject <br>
+ const person = {
+     FirstName:"ASHISH",
+     LastName :"Verma"
+};
+ `
+
+ const cars = ["saab" , "totoya", "BMW"];
+
+ document.getElementById('demo5').innerHTML=`
+ // Array object <br>
+  const cars = ["saab" , "totoya", "BMW"];
+ `
+
+const date = new  Date ("03-03-2025");
+
+
+document.getElementById('demo6').innerHTML=`
+ // Date object <br>
+const date = new  Date ("03-03-2025");
+
+
+`
+
+ //  let adding the number with string
+
+
+
+ var x = 16 +"Volvo";
+ document.getElementById('demo7').innerHTML=`
+  //  let adding the number with string <br>
+ var x = 16 +"Volvo";<br>
+ result =
+ ${x}
+ 
+ `
+
